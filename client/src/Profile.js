d0104235fba821acd16e6598b62cca41fa73226d
seanthaswan <sean-swan@hotmail.com>
2018-01-31 11:12:26 -0800

    Added ability to render one component/feature at a time from the profile page

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -6,19 +6,82 @@ import Wheel from './user/Wheel';
 
 
 class Profile extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      upload: false,
+      wheel: false,
+      palette: false
+    }
+  }
 
+renderUpload = () => {
+  console.log("Clicked")
+  let base = this
+  base.setState({
+    upload: true,
+    wheel: false,
+    palette: false
+  })
+}
+
+renderWheel = () => {
+  console.log("Clicked")
+    let base = this
+  base.setState({
+    upload: false,
+    wheel: true,
+    palette: false
+  })
+}
+
+renderPalette = () => {
+  console.log("Clicked")
+    let base = this
+  base.setState({
+    upload: false,
+    wheel: false,
+    palette: true
+  })
+}
   render(){
-    if(this.props.user && this.props.user.name){
+    if(this.props.user && this.props.user.name && this.state.upload === false && this.state.wheel === false && this.state.palette === false){
       return (
         <div>
           <h2>HELLO AGAIN {this.props.user.name}!</h2>
           <h4>Your email is {this.props.user.email}</h4>
           <h1>WEEEEE</h1>
-          <Upload />
-          <Palette saved={JSON.stringify(this.props.user.saved)}/>
-          <Wheel user={this.props.user}/>
+          <button onClick={this.renderUpload}>Upload and Detect</button>
+          <button onClick={this.renderWheel}>Explore the Color</button>
+          <button onClick={this.renderPalette}>View saved color palette</button>
         </div>);
+    } else if(this.props.user && this.props.user.name && this.state.upload === true && this.state.wheel === false && this.state.palette === false) {
+        return(
+        <div>
+          <button onClick={this.renderWheel}>Explore the Color</button>
+          <button onClick={this.renderPalette}>View saved color palette</button>
+          <h1>Upload then Detect Colors</h1>
+          <Upload />
+        </div>
+      )
+    } else if(this.props.user && this.props.user.name && this.state.upload === false && this.state.wheel === true && this.state.palette === false) {
+        return(
+        <div>
+         <button onClick={this.renderUpload}>Upload and Detect</button>
+          <button onClick={this.renderPalette}>View saved color palette</button>
+          <h1>Color Explorer</h1>
+          <Wheel user={this.props.user}/>
+        </div>
+      )
+    }  else if(this.props.user && this.props.user.name && this.state.upload === false && this.state.wheel === false && this.state.palette === true) {
+        return(
+        <div>
+          <button onClick={this.renderUpload}>Upload and Detect</button>
+          <button onClick={this.renderWheel}>Explore the Color</button>
+          <h1>Favorite Colors</h1>
+          <Palette saved={JSON.stringify(this.props.user.saved)}/>
+        </div>
+      )
     }
     else {
       return (<p>This is a profile page. You need to be logged in to view it.</p>);
