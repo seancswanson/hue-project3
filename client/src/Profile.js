668c1bd6427802a116a1c071a977412370493e8a
jaderosse <jademrosse@gmail.com>
2018-01-31 10:25:12 -0800

    functioning database and selected state is rendering for each user.

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -6,14 +6,17 @@ import Wheel from './user/Wheel';
 
 
 class Profile extends Component {
+  
+
   render(){
     if(this.props.user && this.props.user.name){
       return (
         <div>
           <h2>HELLO AGAIN {this.props.user.name}!</h2>
           <h4>Your email is {this.props.user.email}</h4>
+          <h1>WEEEEE</h1>
           <Upload />
-          <Palette />
+          <Palette saved={JSON.stringify(this.props.user.saved)}/>
           <Wheel user={this.props.user}/>
         </div>);
     }
--- client/src/user/Palette.js
+++ client/src/user/Palette.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
+import Profile from '../Profile';
 
 class Palette extends Component {
 	render(){
+		let array1 = []
+		console.log(this.props.saved);
+    	JSON.parse(this.props.saved).forEach(item => array1.push(item.selected))
 		return(
 			<div>
-				<h1>Palette!</h1>
-				<ul>
-					<li></li>
-				</ul>
+				{array1}
 			</div>
 		)
 	}
