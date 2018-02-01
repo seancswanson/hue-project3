9f1214da11b4da57bae6e72ff7258cd3e8c6f3b4
seanthaswan <sean-swan@hotmail.com>
2018-02-01 15:54:38 -0800

    Uploaded logo to cloudinary to be used in the footer and home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
     return (
     	<div>
         <div className="div--container__home">
-          <p><img className="img--home__logo" src="https://cdn.pixabay.com/photo/2014/09/21/18/22/color-455365_640.png" /></p>
+          <p><img className="img--home__logo" src="http://res.cloudinary.com/huecloud/image/upload/v1517519224/huelogo_yufyyq.png" /></p>
           <h2 className="h2--profile">Welcome to Hue </h2>
           <h2 className="h2--profile__droid">a color detection utility for designers, by designers</h2>
           <h3 className="h3--profile">Been here before?</h3>
